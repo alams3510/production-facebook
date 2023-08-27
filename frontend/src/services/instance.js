610138e7e35253b0f19e101bcab615d4697fc37b
alams3510 <alams3510@gmail.com>
2023-08-27 11:08:02 +0530

    frontend added

--- frontend/src/services/instance.js
+++ frontend/src/services/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: "https://facebuk-app.onrender.com/api/",
 });
 // "http://13.233.90.168:3001/api/"
 // process.env.REACT_APP_BASE_URL
