c97c682ab0c34a2dda1b6252b2caefbe85f469d6
alams3510 <alams3510@gmail.com>
2024-05-23 12:48:34 +0530

    routing improved

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Feed from "../../components/feed/Feed";
 import RightBar from "../../components/rightbar/RightBar";
 import SideBar from "../../components/sideBar/SideBar";
 import TopBar from "../../components/topbar/TopBar";
 import "./home.css";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
+
   const [postUpdateLike, setPostUpdateLike] = useState(0);
+  useEffect(() => {
+    if (user == null) {
+      navigate("/login");
+    }
+  }, [user]);
   return (
     <>
       <TopBar setPostUpdateLike={setPostUpdateLike} />
