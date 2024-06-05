eea8a316de6ab04ce26790e69bee46df97db03e2
alams3510 <alams3510@gmail.com>
2024-06-05 17:34:35 +0530

    final changes

--- redis.js
+++ redis.js
@@ -1,12 +1,17 @@
 const { createClient } = require("ioredis");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const client = createClient({
   password: process.env.REDIS_PASSWORD,
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    connectTimeout: 10000,
-  },
+  // socket: {
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  // connectTimeout: 10000,
+  // tls: process.env.REDIS_TLS === "true" ? {} : undefined, // Use TLS if required
+
+  // },
 });
 
 client.on("connect", () => {
